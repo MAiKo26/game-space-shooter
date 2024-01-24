ad8a8754a80308938fbbee913bd17b55902ff438
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-01-24 19:19:34 +0100

    amunition added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,24 +31,77 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.MoveBackgroundTimer = new System.Windows.Forms.Timer(this.components);
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.MoveLeftTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveForwardTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveRightTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveBackTimer = new System.Windows.Forms.Timer(this.components);
+            this.MyAmunitionTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // MoveBackgroundTimer
             // 
+            this.MoveBackgroundTimer.Enabled = true;
             this.MoveBackgroundTimer.Interval = 10;
             this.MoveBackgroundTimer.Tick += new System.EventHandler(this.MoveBackgroundTimer_Tick);
+            // 
+            // Player
+            // 
+            this.Player.Image = ((System.Drawing.Image)(resources.GetObject("Player.Image")));
+            this.Player.Location = new System.Drawing.Point(258, 390);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(50, 50);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // MoveLeftTimer
+            // 
+            this.MoveLeftTimer.Interval = 5;
+            this.MoveLeftTimer.Tick += new System.EventHandler(this.MoveLeftTimer_Tick);
+            // 
+            // MoveForwardTimer
+            // 
+            this.MoveForwardTimer.Interval = 5;
+            this.MoveForwardTimer.Tick += new System.EventHandler(this.MoveForwardTimer_Tick);
+            // 
+            // MoveRightTimer
+            // 
+            this.MoveRightTimer.Interval = 5;
+            this.MoveRightTimer.Tick += new System.EventHandler(this.MoveRightTimer_Tick);
+            // 
+            // MoveBackTimer
+            // 
+            this.MoveBackTimer.Interval = 5;
+            this.MoveBackTimer.Tick += new System.EventHandler(this.MoveBackTimer_Tick);
+            // 
+            // MyAmunitionTimer
+            // 
+            this.MyAmunitionTimer.Enabled = true;
+            this.MyAmunitionTimer.Interval = 20;
+            this.MyAmunitionTimer.Tick += new System.EventHandler(this.MyAmunitionTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
             this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Player);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(600, 500);
             this.MinimumSize = new System.Drawing.Size(600, 500);
             this.Name = "Form1";
             this.Text = "Space Shooter";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +109,13 @@
         #endregion
 
         private System.Windows.Forms.Timer MoveBackgroundTimer;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer MoveLeftTimer;
+        private System.Windows.Forms.Timer MoveForwardTimer;
+        private System.Windows.Forms.Timer MoveRightTimer;
+        private System.Windows.Forms.Timer MoveBackTimer;
+        private System.Windows.Forms.Timer MyAmunitionTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,12 @@ namespace space_shooter
     public partial class Form1 : Form { 
 
         PictureBox[] stars;
+        PictureBox[] amunitions;
+
         int backgroundspeed;
+        int playerSpeed;
+        int amunitionsSpeed;
+
         Random rnd;
     
         public Form1()
@@ -25,7 +30,25 @@ namespace space_shooter
         {
             backgroundspeed = 4;
             stars = new PictureBox[10];
+
+            playerSpeed = 2;
+
             rnd = new Random();
+
+            amunitions = new PictureBox[4];
+            amunitionsSpeed = 1;
+            Image amunition = Image.FromFile(@"assets\munition.png");
+
+
+            for (int i = 0; i < amunitions.Length; i++)
+            {
+                amunitions[i] = new PictureBox();
+                amunitions[i].Size = new Size(9, 9);
+                amunitions[i].Image = amunition;
+                amunitions[i].SizeMode = PictureBoxSizeMode.Zoom;
+                amunitions[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(amunitions[i]);
+            }
 
             for (int i = 0; i<stars.Length; i++)
             {
@@ -48,7 +71,105 @@ namespace space_shooter
 
         private void MoveBackgroundTimer_Tick(object sender, EventArgs e)
         {
-            for(int i = 0; i<stars.length)
+            for (int i = 0; i < stars.Length/2; i++)
+            {
+                stars[i].Top += backgroundspeed;
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+
+                }
+            }
+            for (int i = stars.Length / 2; i < stars.Length ; i++)
+            {
+                stars[i].Top += backgroundspeed-2;
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+
+                }
+            }
         }
+
+        private void MoveLeftTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Left > 10)
+            {
+                Player.Left -= playerSpeed;
+            }
+        }
+
+        private void MoveForwardTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top > 10)
+            {
+                Player.Top -= playerSpeed;
+            }
+        }
+
+        private void MoveRightTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Right < 580)
+            {
+                Player.Left += playerSpeed;
+            }
+        }
+
+        private void MoveBackTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top < 400)
+            {
+                Player.Top += playerSpeed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                MoveLeftTimer.Start();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                MoveRightTimer.Start();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                MoveForwardTimer.Start();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                MoveBackTimer.Start();
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            MoveRightTimer.Stop();
+            MoveLeftTimer.Stop();
+            MoveForwardTimer.Stop();
+            MoveBackTimer.Stop();
+
+        }
+
+        private void MyAmunitionTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < amunitions.Length; i++)
+            {
+                if (amunitions[i].Top > 0)
+                {
+                    amunitions[i].Visible = true;
+                    amunitions[i].Top -= amunitionsSpeed;
+                }
+                else
+                {
+                    amunitions[i].Visible = false;
+                    amunitions[i].Location = new Point(Player.Location.X + 20, Player.Location.Y - i * 30);
+                }
+            }
+        }
+
+      
     }
 }
